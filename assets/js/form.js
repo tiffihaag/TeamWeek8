8af57cd58c8954b564527de77c42b4d06250b26d
Michael Flasco <mcflasco@gmail.com>
2016-08-16 14:08:48 -0400

    date picker and firebase mod

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,51 +1,66 @@
-var config = {
-    apiKey: "AIzaSyBy0V5ES1QyLAT-p6EtG339Lzyem5HgPG0",
-    authDomain: "foodtruck-adcc6.firebaseapp.com",
-    databaseURL: "https://foodtruck-adcc6.firebaseio.com",
-    storageBucket: "foodtruck-adcc6.appspot.com",
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDbm7Gj4IrW52itBHXsV4R5wRG5rOGnWzU",
+    authDomain: "first-new-project.firebaseapp.com",
+    databaseURL: "https://first-new-project.firebaseio.com",
+    storageBucket: "first-new-project.appspot.com",
   };
-  firebase.initializeApp(config);  
+  firebase.initializeApp(config);
 
   var database = firebase.database();
 
  // Declaring onclick function //
- $("#search").on("click", function(){
-  console.log("it works");
+$("#search").on("click", function(){
+    console.log("it works");
 	// displays in firebase upside down //
     var name = "";
     var business = "";
     var location = "";
-    var dateTime = "";
+    var date= "";
     var menu = "";
 	name = $('#name').val().trim();
 	business = $('#business').val().trim();
     location = $('#location').val().trim();
-	dateTime = $('#dateTime').val().trim();
+	date = $('#datepicker').val();
 	menu = $('#menu').val().trim();
+
 
 	database.ref().push({
 		name: name,
 		business: business,
 		location: location,
-		dateTime: dateTime,
-    menu: menu
+		date: date,
+        menu: menu
 
 	});
 	return false;
  })
-  database.ref().on("child_added", function(snapshot){
+database.ref().on("child_added", function(snapshot){
     // console.log //
-  console.log(snapshot.val());
-  console.log(snapshot.val().name);
-  console.log(snapshot.val().business);
-  console.log(snapshot.val().location);
-  console.log(snapshot.val().dateTime);
-  console.log(snapshot.val().menu)
+    console.log(snapshot.val());
+    console.log(snapshot.val().name);
+    console.log(snapshot.val().business);
+    console.log(snapshot.val().location);
+    console.log(snapshot.val().date);
+    console.log(snapshot.val().menu)
 
 
     var name = snapshot.val().name
   var business = snapshot.val().business
   var location = snapshot.val().location
-  var dateTime = snapshot.val().dateTime
+  var dateTime = snapshot.val().date
   var menu = snapshot.val().menu
 });
+
+  $( function() {
+    $( "#datepicker" ).datepicker();
+  });
+
+
+  $("#datepicker").on("click", function(){
+    var dates = [];
+    var date = $("#newDate").html();
+     dates.push(date);
+    
+  });
+
