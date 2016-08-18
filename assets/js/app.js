93abb4f00dfc134061b70f564453afdf938f4197
tiffihaag <tiffihaag@gmail.com>
2016-08-18 15:32:13 -0400

    cleaning up code

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,27 +1,63 @@
-$(".AfterLogin").hide();
+var googleUser = {};
 
-//Firebase
-var config = {
-  apiKey: "AIzaSyBy0V5ES1QyLAT-p6EtG339Lzyem5HgPG0",
-  authDomain: "foodtruck-adcc6.firebaseapp.com",
-  databaseURL: "https://foodtruck-adcc6.firebaseio.com",
-  storageBucket: "foodtruck-adcc6.appspot.com",
-};
-firebase.initializeApp(config);
+  var startApp = function() {
+    gapi.load('auth2', function(){
+      // Retrieve the singleton for the GoogleAuth library and set up the client.
+      auth2 = gapi.auth2.init({
+        client_id: '480535686459-1cn1iqo3np6uf08n9o5nmdto4vcm9pls.apps.googleusercontent.com',
+        cookiepolicy: 'none',
+        // Request scopes in addition to 'profile' and 'email'
+        //scope: 'additional_scope'
+      });
+      attachSignin(document.getElementById('customBtn'));
+    });
+  };
 
-var database = firebase.database();
+  function attachSignin(element) {
+    console.log(element.id);
+    auth2.attachClickHandler(element, {},
+      function(googleUser) {
+        document.getElementById('name').innerText = "Signed in: " +
+              googleUser.getBasicProfile().getName();
+        }, function(error) {
+          alert(JSON.stringify(error, undefined, 2));
+        });
+  }
 
-//Google Sign In
+  function onSuccess(googleUser) {
+    var profile = googleUser.getBasicProfile();
+    gapi.client.load('plus', 'v1', function () {
+        var request = gapi.client.plus.people.get({
+            'userId': 'me'
+        });
+
+        //Display the user details
+        request.execute(function (resp) {
+            var profileHTML = '<div class="profile"><div class="head">Welcome '+resp.name.givenName+'! <a href="javascript:void(0);" onclick="signOut();">Sign out</a></div>';
+            profileHTML += '<img src="'+resp.image.url+'"/><div class="proDetails"><p>'+resp.displayName+'</p><p>'+resp.emails[0].value+'</p><p>'+resp.gender+'</p><p>'+resp.id+'</p><p><a href="'+resp.url+'">View Google+ Profile</a></p></div></div>';
+            $('.userContent').html(profileHTML);
+            $('#gSignIn').slideUp('slow');
+        });
+      $(".AfterLogin").show();
+      $(".Login").hide();
+    });
+}
+
+function onFailure(error) {
+    alert(error);
+    $(".AfterLogin").hide();
+}
+
 function renderButton() {
-  gapi.signin2.render('my-signin2', {
-    'scope': 'profile email',
-    'width': 240,
-    'height': 50,
-    'longtitle': true,
-    'theme': 'dark',
-    'onsuccess': onSuccess,
-    'onfailure': onFailure
-  });
+    gapi.signin2.render('gSignIn', {
+        'scope': 'profile email',
+        'width': 240,
+        'height': 50,
+        'longtitle': true,
+        'theme': 'dark',
+        'onsuccess': onSuccess,
+        'onfailure': onFailure
+    });
 }
 
 function onSignIn(googleUser) {
@@ -42,6 +78,17 @@ function onSuccess(googleUser) {
   console.log(error);
   $(".AfterLogin").hide();
   }
+
+//Firebase
+var config = {
+  apiKey: "AIzaSyBy0V5ES1QyLAT-p6EtG339Lzyem5HgPG0",
+  authDomain: "foodtruck-adcc6.firebaseapp.com",
+  databaseURL: "https://foodtruck-adcc6.firebaseio.com",
+  storageBucket: "foodtruck-adcc6.appspot.com",
+};
+firebase.initializeApp(config);
+
+var database = firebase.database();
 
 //Google Map
 var map;
