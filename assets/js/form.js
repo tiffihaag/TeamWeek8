4b3341286cc62a42c1ad0bf58faf92159814cb5a
Michael Flasco <mcflasco@gmail.com>
2016-08-19 19:27:52 -0400

    form complete, minor css changes needed

--- assets/js/form.js
+++ assets/js/form.js
@@ -15,27 +15,44 @@ $("#search").on("click", function(){
 	// displays in firebase upside down //
 
     var name = "";
+    var email = "";
     var business = "";
-    var location = "";
+    var address = "";
+    var city = "";
+    var state = "";
+    var zip = "";
     var date = "";
-    var time = "";
+    var start = "";
+    var end = "";
     var menu = "";
+    var comment = "";
 
 	name = $('#name').val().trim();
+	email = $("#email").val().trim();
 	business = $('#business').val().trim();
-    location = $('#location').val().trim();
-	date = $('#datepicker').val();
-    time = $('#timeChoice').val();
+    address = $('#address').val().trim();
+    city = $("#city").val().trim();
+    state = $('#state').val().trim();
+    zip = $('#zip').val().trim();
+	date = $('#datepicker').val().trim();
+    start = $('#start').val().trim();
+    end = $('#end').val().trim();
 	menu = $('#menu').val().trim();
-
+	comment = $("#comment").val().trim();
 
 	database.ref().push({
 		name: name,
+		email: email,
 		business: business,
-		location: location,
+		address: address,
+		city: city,
+		state: state,
+		zip: zip,
 		date: date,
-        time: time,
-        menu: menu
+        start: start,
+        end: end,
+        menu: menu,
+        comment: comment
 
 	});
 	return false;
@@ -45,19 +62,31 @@ database.ref().on("child_added", function(snapshot){
     // console.log //
     console.log(snapshot.val());
     console.log(snapshot.val().name);
+    console.log(snapshot.val().email);
     console.log(snapshot.val().business);
-    console.log(snapshot.val().location);
+    console.log(snapshot.val().address);
+    console.log(snapshot.val().city);
+    console.log(snapshot.val().state);
+    console.log(snapshot.val().zip);
     console.log(snapshot.val().date);
-    console.log(snapshot.val().time);
-    console.log(snapshot.val().menu)
+    console.log(snapshot.val().start);
+    console.log(snapshot.val().end);
+    console.log(snapshot.val().menu);
+    console.log(snapshot.val().comment)
 
 
-    var name = snapshot.val().name
+  var name = snapshot.val().name
+  var email = snapshot.val().email
   var business = snapshot.val().business
-  var location = snapshot.val().location
+  var address = snapshot.val().address
+  var city = snapshot.val().city
+  var state = snapshot.val().state
+  var zip = snapshot.val().zip
   var dateTime = snapshot.val().date
-  var timeChoice = snapshot.val().time
+  var start = snapshot.val().start
+  var end = snapshot.val().end
   var menu = snapshot.val().menu
+  var comment = snapshot.val().comment
 });
 
   $( function() {
@@ -66,20 +95,13 @@ database.ref().on("child_added", function(snapshot){
 
 
     $(function() {
-    $('#timeChoice').timepicker();
+    $('#start').timepicker();
      });
 
     $(function() {
-    $('#timeEnd').timepicker();
+    $('#end').timepicker();
      });
 
 
-
-  $("#datepicker").on("click", function(){
-    var dates = [];
-    var date = $("#newDate").html();
-     dates.push(date);
-  });
-           
-
+ 
 
