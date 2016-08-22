7959f8f5cb62f189ff6ab69fd013b941514c3462
tiffihaag <tiffihaag@gmail.com>
2016-08-22 16:34:01 -0400

    SearchBarTiff

--- assets/js/form.js
+++ assets/js/form.js
@@ -9,6 +9,8 @@
 
   var database = firebase.database();
 
+$(document).ready(function(){
+
  // Declaring onclick function //
 $("#search").on("click", function(){
     console.log("it works");
@@ -27,121 +29,102 @@ $("#search").on("click", function(){
     var menu = "";
     var comment = "";
 
-	name = $('#name').val().trim();
-	email = $("#email").val().trim();
-	business = $('#business').val().trim();
-  address = $('#address').val().trim();
-  city = $("#city").val().trim();
-  state = $('#state').val().trim();
-  zip = $('#zip').val().trim();
-	date = $('#datepicker').val().trim();
-  start = $('#start').val().trim();
-  end = $('#end').val().trim();
-	menu = $('#menu').val().trim();
-	comment = $("#comment").val().trim();
+  	name = $('#name').val().trim();
+  	email = $("#email").val().trim();
+  	business = $('#business').val().trim();
+    address = $('#address').val().trim();
+    city = $("#city").val().trim();
+    state = $('#state').val().trim();
+    zip = $('#zip').val().trim();
+  	date = $('#datepicker').val().trim();
+    start = $('#start').val().trim();
+    end = $('#end').val().trim();
+  	menu = $('#menu').val().trim();
+  	comment = $("#comment").val().trim();
 
-	database.ref().push({
-		name: name,
-		email: email,
-		business: business,
-		address: address,
-		city: city,
-		state: state,
-		zip: zip,
-		date: date,
-    start: start,
-    end: end,
-    menu: menu,
-    comment: comment
-	});
+  	database.ref().push({
+  		name: name,
+  		email: email,
+  		business: business,
+  		address: address,
+  		city: city,
+  		state: state,
+  		zip: zip,
+  		date: date,
+      start: start,
+      end: end,
+      menu: menu,
+      comment: comment
+  	}); //ref.push
 
-	return false;
+  	return false;
 
-database.ref().on("child_added", function(snapshot){
-    // console.log //
-    console.log(snapshot.val());
-    console.log(snapshot.val().name);
-    console.log(snapshot.val().email);
-    console.log(snapshot.val().business);
-    console.log(snapshot.val().address);
-    console.log(snapshot.val().city);
-    console.log(snapshot.val().state);
-    console.log(snapshot.val().zip);
-    console.log(snapshot.val().date);
-    console.log(snapshot.val().start);
-    console.log(snapshot.val().end);
-    console.log(snapshot.val().menu);
-    console.log(snapshot.val().comment)
+    database.ref().on("child_added", function(snapshot){
+      // console.log //
+      console.log(snapshot.val());
+      console.log(snapshot.val().name);
+      console.log(snapshot.val().email);
+      console.log(snapshot.val().business);
+      console.log(snapshot.val().address);
+      console.log(snapshot.val().city);
+      console.log(snapshot.val().state);
+      console.log(snapshot.val().zip);
+      console.log(snapshot.val().date);
+      console.log(snapshot.val().start);
+      console.log(snapshot.val().end);
+      console.log(snapshot.val().menu);
+      console.log(snapshot.val().comment)
+
+      var name = snapshot.val().name
+      var email = snapshot.val().email
+      var business = snapshot.val().business
+      var address = snapshot.val().address
+      var city = snapshot.val().city
+      var state = snapshot.val().state
+      var zip = snapshot.val().zip
+      var dateTime = snapshot.val().date
+      var start = snapshot.val().start
+      var end = snapshot.val().end
+      var menu = snapshot.val().menu
+      var comment = snapshot.val().comment
+      }); //child added
+  }); //onclick function
+
+    $(function() {
+      $("#datepicker").datepicker();
+    });
+
+    $(function() {
+      $('#start').timepicker();
+    });
+
+    $(function() {
+      $('#end').timepicker();
+    });
+
+    //Stuff For the Search Bar on index.html
+    $("#searchDatabase").on("click", function(){
+      var cityName = $("#searchMe").val();
+      console.log("hello");
+      console.log("From database: " + cityName);
+
+    database.ref().on("child_added", function(childSnapshot) {
+      console.log("checking this " + childSnapshot.val().city);
+      var cityInData = (childSnapshot.val().city);
+
+      if (cityInData = cityName) {
+        database.ref().on("value", function(snapshot) {
+        console.log ("I got this");
+      }); //database.ref on value function
+
+        $('.searchResults').append("<div class='tableStuff'><span id='tableName'> "+childSnapshot.val().name+" </span></br><span id='tableEmail'> "+childSnapshot.val().email+"</span></br><span id='tableBusiness'> "+childSnapshot.val().business+"</span></br><span id='tableAddress'> "+childSnapshot.val().address+"</span></br><span id='tableCity'> "+childSnapshot.val().city+"</span></br><span id='tableState'> "+childSnapshot.val().state+"</span></br><span id='tableZip'> "+childSnapshot.val().zip+"</span></br><span id='tableDateTime'> "+childSnapshot.val().dateTime+"</span><span id='tableStart'> "+childSnapshot.val().start+"</span></br><span id='tableEnd'> "+childSnapshot.val().end+"</span></br><span id='tableComment'> "+childSnapshot.val().comment+"</span></div>");
+        
+      }; //city equals city in database
+    }); //for child_added childSnapshot
+  }); //for searchDatabase on click
+}); //doc.ready function
 
 
-  var name = snapshot.val().name
-  var email = snapshot.val().email
-  var business = snapshot.val().business
-  var address = snapshot.val().address
-  var city = snapshot.val().city
-  var state = snapshot.val().state
-  var zip = snapshot.val().zip
-  var dateTime = snapshot.val().date
-  var start = snapshot.val().start
-  var end = snapshot.val().end
-  var menu = snapshot.val().menu
-  var comment = snapshot.val().comment
-  }); //child added
-}); //onclick
-
-  $( function() {
-    $("#datepicker").datepicker();
-  });
-
-  $(function() {
-    $('#start').timepicker();
-  });
-
-  $(function() {
-    $('#end').timepicker();
-  });
-
-
-//For the Search Bar
-$("#searchDatabase").on("click", function(){
-  var cityName = $(".searchBar").val();
-  console.log("Whaaaaa");
-  var tname = childSnapshot.val().name;
-  var temail = childSnapshot.val().email;
-  var tbusiness = childSnapshot.val().business;
-  var taddress = childSnapshot.val().address;
-  var tcity = childSnapshot.val().city;
-  var tstate = childSnapshot.val().state;
-  var tzip = childSnapshot.val().zip;
-  var tdateTime = childSnapshot.val().dateTime;
-  var tstart = childSnapshot.val().start;
-  var tend = childSnapshot.val().end;
-  var tmenu = childSnapshot.val().menu;
-  var tcomment = childSnapshot.val().comment;
-
-  for (var i = 0; i < child.length; i++) {
-    if (cityName == tcity) {
-      var tableElem = $('<tr>').attr("dataName", childSnapshot.val().trainName);
-      tableElem.append($('<td>').text(childSnapshot.val().tname));
-      tableElem.append($('<td>').text(childSnapshot.val().temail));
-      tableElem.append($('<td>').text(childSnapshot.val().tbusiness));
-      tableElem.append($('<td>').text(childSnapshot.val().taddress));
-      tableElem.append($('<td>').text(childSnapshot.val().tcity));
-      tableElem.append($('<td>').text(childSnapshot.val().tstate));
-      tableElem.append($('<td>').text(childSnapshot.val().tzip));
-      tableElem.append($('<td>').text(childSnapshot.val().tdateTime));
-      tableElem.append($('<td>').text(childSnapshot.val().tstart));
-      tableElem.append($('<td>').text(childSnapshot.val().tend));
-      tableElem.append($('<td>').text(childSnapshot.val().tmenu));
-      tableElem.append($('<td>').text(childSnapshot.val().tcomment));
-      $('#tBody').append(tableElem);
-    }
-  else {
-    console.log("Not a city");
-  }
-
-  } //for closure
-}); //searchDatabase
 
 
   
