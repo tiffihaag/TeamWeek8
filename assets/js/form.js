d9b4c993a22374c1e9fec229559b7156d21d2fe3
tiffihaag <tiffihaag@gmail.com>
2016-08-25 15:35:09 -0400

    map desc

--- assets/js/form.js
+++ assets/js/form.js
@@ -105,9 +105,22 @@ $("#search").on("click", function(){
 
     //Stuff For the Search Bar on index.html
     $("#searchDatabase").on("click", function(){
+      $("#tableName").empty("");
+      $("#tableEmail").empty("");
+      $("#tableBusiness").empty("");
+      $("#tableAddress").empty("");
+      $("#tableCity").empty("");
+      $("#tableState").empty("");
+      $("#tableZip").empty("");
+      $("#tableDateTime").empty("");
+      $("#tableStart").empty("");
+      $("#tableEnd").empty("");
+      $("#tableComment").empty("");
+
       var cityName = $("#searchMe").val();
       console.log("hello");
       console.log("From database: " + cityName);
+      
 
     database.ref().on("child_added", function(childSnapshot) {
       console.log("checking this " + childSnapshot.val().city);
@@ -117,7 +130,7 @@ $("#search").on("click", function(){
         database.ref().on("value", function(snapshot) {
         console.log ("Works even though it's one equal sign");
       }); //database.ref on value function
-        $('.searchResults').append("<div class='tableStuff'><span id='tableName'><b>"+childSnapshot.val().name+"</b> </span></br><span id='tableEmail'> "+childSnapshot.val().email+"</span></br><span id='tableBusiness'> "+childSnapshot.val().business+"</span></br><span id='tableAddress'> "+childSnapshot.val().address+"</span></br><span id='tableCity'> "+childSnapshot.val().city+"</span></br><span id='tableState'> "+childSnapshot.val().state+"</span></br><span id='tableZip'> "+childSnapshot.val().zip+"</span></br><span id='tableDateTime'> "+childSnapshot.val().date+"</span><span id='tableStart'> "+childSnapshot.val().start+"</span></br><span id='tableEnd'> "+childSnapshot.val().end+"</span></br><span id='tableComment'> "+childSnapshot.val().comment+"</span></br></br></div>");
+        $('.searchResults').append("<div class='tableStuff'><span id='tableName'><br>"+childSnapshot.val().name+"</br> </span></br><span id='tableEmail'> "+childSnapshot.val().email+"</span></br><span id='tableBusiness'> "+childSnapshot.val().business+"</span></br><span id='tableAddress'> "+childSnapshot.val().address+"</span></br><span id='tableCity'> "+childSnapshot.val().city+"</span></br><span id='tableState'> "+childSnapshot.val().state+"</span></br><span id='tableZip'> "+childSnapshot.val().zip+"</span></br><span id='tableDateTime'> "+childSnapshot.val().date+"</span><span id='tableStart'> "+childSnapshot.val().start+"</span></br><span id='tableEnd'> "+childSnapshot.val().end+"</span></br><span id='tableComment'> "+childSnapshot.val().comment+"</span></br></br></div>");
         
       }; //city equals city in database
     }); //for child_added childSnapshot
